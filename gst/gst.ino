8992995703cc944f438429e7b67f3d7ac7c61431
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2022-09-04 21:14:37 -0600

    Initial commit

---
+++ gst/gst.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+uint8_t packet[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0xFF};
+
+void setup() {
+  // serial configuration
+  Serial.begin(9600);
+  while(!Serial);  //not needed in real GST
+
+  // LoRa configuration
+  if (LoRa.begin(434E6)) {
+    Serial.println("LoRa successful!");
+  }else {
+    Serial.println("LoRa not successful!");
+  }
+
+  LoRa.setSpreadingFactor(8);       // from 6 to 12
+  LoRa.setCodingRate4(4);           // from 4 to 8
+  LoRa.setSignalBandwidth(41.7E3);  // values: 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3
+
+  LoRa.setTxPower(18,PA_OUTPUT_PA_BOOST_PIN); //18dbm, 63mW
+
+  delay(500);
+}
+
+void loop() {
+  // 16 byte
+  Serial.println("Sending LoRa packet");
+  LoRa.beginPacket();
+  LoRa.write(packet,16);
+  LoRa.endPacket();
+  Serial.println("LoRa packet transmitted");
+  delay(5000);
+  
+
+}
